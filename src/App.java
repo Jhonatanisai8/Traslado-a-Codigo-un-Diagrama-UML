14f5beebff2d6fdfc0c9a8b8d5579e37a3cabd2c
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-12-22 21:03:13 -0500

    clase main: ejemplo de como obtener calcular la diferencia de años que hay entre dos fechas

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+import java.time.LocalDate;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        LocalDate fechaInicio = LocalDate.of(2013, 12, 12);
+        LocalDate fechaActual = LocalDate.now();
+        int edad =  fechaActual.getYear() -fechaInicio.getYear();
+        System.out.println("fecha: " + fechaInicio);
+        System.out.println("Edad: " + edad);
     }
 }
